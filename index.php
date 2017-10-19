6aa034ae71be0c95427935c81dcf7d44b6c1c0a8
CodeClouds <codeclouds@Tylers-MacBook-Pro.local>
2017-10-19 16:37:24 +1300

    labels in progress

--- index.php
+++ index.php
@@ -2,7 +2,29 @@
 <head>
   
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
-
+<style type="text/css">
+ul{
+  padding:0px;
+  margin:0;
+}
+li{
+  padding:5px;
+  overflow: hidden;
+  max-height: auto;
+  height:auto;
+}
+  li p{
+    font-size:18px;
+    font-family: helvetica;
+    position: relative;
+    display:inline-block;
+    background:rgba(0,0,0,0);
+    top:20px;
+    left:20px;
+padding:0px;
+margin:0;
+  }
+</style>
 
 </head>
 <?php
@@ -13,33 +35,45 @@ include('progress-bar-style.php');
 ?>
 
 <body>
-<div style="max-width:480px; margin:auto; padding-top:20px;">
+<div style="max-width:480px; margin:auto; padding-top:20px; background:rgba(0,0,0,0.2); height:480px;">
   <p style="font-family:helvetica; text-align:center;">JavaScript Progress Bar Array</p>
-
+<ul class="skillslist" style="background:red;">
+  <li><p>test</p>
 <div class="myProgress1">
   <div class="myBar"></div>
 </div>
-<br>
+</li>
+
+<li><p>test</p>
 <div class="myProgress2">
   <div class="myBar"></div>
 </div>
-<br>
+</li>
+
+<li><p>test</p>
 <div class="myProgress3">
   <div class="myBar"></div>
 </div>
-<br>
+</li>
+
+<li><p>test</p>
 <div class="myProgress4">
   <div class="myBar"></div>
 </div>
-<br>
+</li>
+
+<li><p>test</p>
 <div class="myProgress5">
   <div class="myBar"></div>
 </div>
-<br>
+</li>
+
+<li><p>test</p>
 <div class="myProgress6">
   <div class="myBar"></div>
 </div>
-<br>
+</li>
+</ul>
 </div>
 
 <br>
@@ -56,20 +90,6 @@ setTimeout(fadeIn, 500);
 function fadeOut(){
   $('.myBar').removeClass('myBarFill')
 }
-
-// function move() {
-//   var elem = document.getElementById("myBar");   
-//   var width = 1;
-//   var id = setInterval(frame, 10);
-//   function frame() {
-//     if (width >= 100) {
-//       clearInterval(id);
-//     } else {
-//       width++; 
-//       elem.style.width = width + '%'; 
-//     }
-//   }
-// }
 
 
 </script>
--- progress-bar-style.php
+++ progress-bar-style.php
@@ -1,16 +1,108 @@
 <?php
-
-// total number of bars within the array - used for array control 
-$total = 6+1;
+switch (4) {
+    case 2:
+    $cs_00="#F2F2F2";
+    $cs_01="#F2F2F2";
+    $cs_02="#2F343B";
+    $cs_03="#405F59";
+    $cs_04="#A3D0C1";
+    $font_color="#000";
+    $font_color2="#fff";
+    $cs_hi = "#A3D0C1";
+    $cs_tint = "#A3D0C1";
+    $cs_texture = "https://www.toptal.com/designers/subtlepatterns/patterns/round.png";
+    $schematic = "mellow";
+    break;
+    case 3:
+    $cs_00="#1b1f20";
+    $cs_01="#242C38";
+    $cs_02="#2F343B";
+    $cs_03="#7E827A";
+    $cs_04="#7E827A";
+    $font_color="#fff";
+    $font_color2="#fff";
+    $cs_hi = "#FD775A";
+    $cs_tint = "#FD775A";
+    $cs_texture = "https://www.toptal.com/designers/subtlepatterns/patterns/curls.png";
+    $schematic = "vintage";
+    break;
+    case 4:
+    $cs_00="#324D5C";
+    $cs_01="#46B29D";
+    $cs_02="#F0CA4D";
+    $cs_03="#E37B40";
+    $cs_04="#DE5B49";
+    $font_color="#fff";
+    $font_color2="#000";
+    $cs_hi = "#F0CA4D";
+    $cs_tint = "#F0CA4D";
+    $cs_texture = "https://www.toptal.com/designers/subtlepatterns/patterns/round.png";
+    $schematic = "pineapple";
+    break;
+     case 5:
+    $cs_00="#DBEFE7";
+    $cs_01="#B2C8C6";
+    $cs_02="#F2F3F2";
+    $cs_03="#0b2b35";
+    $cs_04="#D6CCBC";
+    $font_color="#000";
+    $font_color2="#000";
+    $cs_hi = "#0b2b35";
+    $cs_tint = "#0b2b35";
+    $cs_texture = "https://www.toptal.com/designers/subtlepatterns/patterns/round.png";
+    $schematic = "blanca";
+    break;
+    case 6:
+    $cs_00="#1F618C";
+    $cs_01="#2C3E50";
+    $cs_02="#E74C3C";
+    $cs_03="#ECF0F1";
+    $cs_04="#1F618C";
+    $font_color="#fff";
+    $font_color2="#fff";
+    $cs_hi = "#E74C3C";
+    $cs_tint = "#ffffff";
+    $cs_texture = "https://www.toptal.com/designers/subtlepatterns/patterns/round.png";
+    $schematic = "honeypot";
+    break;
+     case 7:
+    $cs_00="#272F32";
+    $cs_01="#9DBDC6";
+    $cs_02="#FFFFFF";
+    $cs_03="#FF3D2E";
+    $cs_04="#DAEAEF";
+    $font_color="#000";
+    $font_color2="#000";
+    $cs_hi = "#FF3D2E";
+    $cs_tint = "#FF3D2E";
+    $cs_texture = "https://www.toptal.com/designers/subtlepatterns/patterns/round.png";
+    $schematic = "futurama";
+    break;
+    default:
+    $cs_00="#DBEFE7";
+    $cs_01="#C1D4D2";
+    $cs_02="#F2F3F2";
+    $cs_03="#A69B8C";
+    $cs_04="#D6CCBC";
+    $font_color="#fff";
+    $font_color2="#1b1f20";
+    $cs_hi = "#4CC1BB";
+    $cs_tint = "#DBEFE7";
+    $cs_texture = "https://www.toptal.com/designers/subtlepatterns/patterns/round.png";
+    $schematic = "arctic";
+}
 
 $config = array(
-"bar1" => "10",
+"bar1" => "40",
 "bar2" => "60",
 "bar3" => "100",
 "bar4" => "60",
 "bar5" => "70",
 "bar6" => "45"
 );
+
+// total number of bars within the array (+1 for array) - used for array control 
+$total = 6+1;
 
 
 echo "
@@ -22,11 +114,11 @@ for ($i=0; $i<=$total; $i++){
   echo "
 .myProgress$i {
   width: 50%;
-  margin:auto;
-  background-color: rgba(0,0,0,0.1);
+  margin-left:40%;
+  background-color: rgba(0,0,0,0.8);
   border-radius:20px;
   overflow: hidden;
-  border:2px solid #1b1f20;
+  border:2px solid transparent;
 }
 ";
 }
@@ -37,7 +129,7 @@ echo "
   transition:0.9s ease all;
   width: 1%;
   height: 30px;
-  background-color: #8cce82;
+  background-color: $cs_00;
   border:2px solid none;
   border-radius:20px;
   border-top-left-radius: 0px;
@@ -55,11 +147,10 @@ echo "
   border-bottom-left-radius: 0px;
   
 background: green; /* Old browsers */
-background: -moz-linear-gradient(left, #8cce82 0%, #75aad6 50%, #ba82f2 100%); /* FF3.6-15 */
-background: -webkit-linear-gradient(left, #8cce82 0%,#75aad6 50%,#ba82f2 100%); /* Chrome10-25,Safari5.1-6 */
-background: linear-gradient(to right, #8cce82 0%,#75aad6 50%,#ba82f2 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#8cce82', endColorstr='#ba82f2',GradientType=1 ); /* IE6-9 */
-
+background: -moz-linear-gradient(left, $cs_00 0%, $cs_04 50%, $cs_hi 100%); /* FF3.6-15 */
+background: -webkit-linear-gradient(left, $cs_00 0%,$cs_04 50%,$cs_hi 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(to right, $cs_00 0%,$cs_04 50%,$cs_hi 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='$cs_00', endColorstr='$cs_hi',GradientType=1 ); /* IE6-9 */
 }
 ";
 
